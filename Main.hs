8215a959eb74f38a98031ed11e25a7ae80032091
David Johnson <djohnson.m@gmail.com>
2020-11-30 23:21:48 -0600

    Day 1

--- Main.hs
+++ Main.hs
@@ -3,19 +3,42 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
+import Control.Monad   (sequence)
+import Data.List       (nub, tails)
 import Data.List.Split (splitOn)
-import Data.List
 
 type Parsed = Int
 
-main' :: IO ()
-main' = process =<< fmap parse . lines <$> readFile "input.txt"
+readInput :: String -> IO [Parsed]
+readInput name = fmap read . lines <$> readFile name
 
-parse :: String -> Parsed
-parse = read
+day1 :: IO ()
+day1 = sequence_ [ day1pt1, day1pt2 ]
 
-process :: [Parsed] -> IO ()
-process _ = pure ()
+day1pt1 :: IO ()
+day1pt1 = print =<< processDay1Pt1 <$> readInput "day1.txt"
+
+day1pt2 :: IO ()
+day1pt2 = print =<< processDay1Pt2 <$> readInput "day1.txt"
+
+processDay1Pt1 :: [Parsed] -> Parsed
+processDay1Pt1 input
+  = head
+  [ x * y
+  | x : xs <- tails input
+  , y <- xs
+  , x + y == 2020
+  ]
+
+processDay1Pt2 :: [Parsed] -> Parsed
+processDay1Pt2 input
+  = head
+  [ x * y * z
+  | x:xs <- tails input
+  , y:ys <- tails xs
+  , z <- ys
+  , x + y + z == 2020
+  ]
 
 main :: IO ()
-main = pure ()
+main = day1
