976353fa96112a36ecdeefc1f3dc8b832d75e655
David Johnson <djohnson.m@gmail.com>
2020-12-03 00:29:32 -0600

    Day 3

--- Main.hs
+++ Main.hs
@@ -1,17 +1,19 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-#LANGUAGE BangPatterns#-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications  #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Data.Bits
-import Control.Monad
-import Data.List
-import Data.List.Split
+import           Control.Monad
+import           Data.Bits
+import           Data.List
+import           Data.List.Split
+import qualified Data.Map.Strict as M
+import           Debug.Trace
 
-type Parsed = Int
-
-readInput :: String -> IO [Parsed]
+readInput :: String -> IO [Int]
 readInput name = fmap read . lines <$> readFile name
 
 day1 :: IO ()
@@ -25,7 +27,7 @@ day1pt1 = print =<< processDay1Pt1 <$> readInput "day1.txt"
 day1pt2 :: IO ()
 day1pt2 = print =<< processDay1Pt2 <$> readInput "day1.txt"
 
-processDay1Pt1 :: [Parsed] -> Parsed
+processDay1Pt1 :: [Int] -> Int
 processDay1Pt1 input
   = head
   [ x * y
@@ -34,7 +36,7 @@ processDay1Pt1 input
   , x + y == 2020
   ]
 
-processDay1Pt2 :: [Parsed] -> Parsed
+processDay1Pt2 :: [Int] -> Int
 processDay1Pt2 input
   = head
   [ x * y * z
@@ -82,6 +84,62 @@ validDay2PartTwo :: String -> Char -> Int -> Int -> Bool
 validDay2PartTwo pw val x1 x2 =
   (pw !! (x1 - 1) == val) `xor` (pw !! (x2 - 1) == val)
 
+day3 :: IO ()
+day3 = do
+  putStrLn "day 3"
+  xs <- lines <$> readFile "day3.txt"
+  let rowSize   = length (head xs)
+      totalRows = length xs
+      mapping   = populateMap xs
+  print $ day3pt1 mapping rowSize
+  print $ day3pt2 mapping rowSize
+
+day3pt1 :: M.Map (Int,Int) Type -> Int -> Int
+day3pt1 = travelAccum (3,1)
+
+day3pt2 :: M.Map (Int,Int) Type -> Int -> Int
+day3pt2 m r =
+  product
+  [ travelAccum (1,1) m r
+  , travelAccum (3,1) m r
+  , travelAccum (5,1) m r
+  , travelAccum (7,1) m r
+  , travelAccum (1,2) m r
+  ]
+
+travelAccum
+  :: (Int,Int)
+  -- ^ map
+  -> M.Map (Int,Int) Type
+  -- ^ x and y accumulators
+  -> Int
+  -- ^ col count
+  -> Int
+travelAccum (xAcc, yAcc) m rowSize = go (0,0) 0
+  where
+    go (!x, !y) !acc = do
+      let newKey = (x + xAcc, y + yAcc)
+      case M.lookup (x `mod` rowSize, y) m of
+        Just Tree ->
+          go newKey (acc + 1)
+        Just Square ->
+          go newKey acc
+        Nothing ->
+          acc
+
+populateMap :: [String] -> M.Map (Int,Int) Type
+populateMap gs = go 0 0 gs mempty
+  where
+    go x y [] m = m
+    go x y (('.':ls):lls) m =
+      go (x+1) y (ls:lls) (M.insert (x,y) Square m)
+    go x y (('#':ls):lls) m =
+      go (x+1) y (ls:lls) (M.insert (x,y) Tree m)
+    go x y ([]:lls) m = go 0 (y+1) lls m
+
+data Type = Square | Tree
+  deriving (Show, Eq)
+
 main :: IO ()
-main = day1 >> day2
+main = day1 >> day2 >> day3
 
