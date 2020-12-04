0a1293114c1c574d9b82a4f94363cd4e3803b11f
David Johnson <djohnson.m@gmail.com>
2020-12-03 23:32:30 -0600

    Day 4

--- Main.hs
+++ Main.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-#LANGUAGE BangPatterns#-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications  #-}
@@ -8,10 +8,13 @@ module Main where
 
 import           Control.Monad
 import           Data.Bits
+import           Data.Char
 import           Data.List
 import           Data.List.Split
 import qualified Data.Map.Strict as M
+import           Data.Maybe
 import           Debug.Trace
+import           Text.Read
 
 readInput :: String -> IO [Int]
 readInput name = fmap read . lines <$> readFile name
@@ -48,7 +51,7 @@ processDay1Pt2 input
 
 day2 :: IO ()
 day2 = do
-  putStrLn "day2"
+  putStrLn "day 2"
   sequence_ [ day2pt1, day2pt2 ]
 
 day2pt1 :: IO ()
@@ -140,6 +143,91 @@ populateMap gs = go 0 0 gs mempty
 data Type = Square | Tree
   deriving (Show, Eq)
 
+day4 :: IO ()
+day4 = do
+  putStrLn "day 4"
+  ls <- lines <$> readFile "day4.txt"
+  let newLines = fmap (intercalate " ") (splitOn [""] ls)
+  print $ day4pt1 newLines
+  print $ day4pt2 newLines
+
+day4pt1 :: [String] -> Int
+day4pt1 = solve4 (const mempty)
+
+day4pt2 :: [String] -> Int
+day4pt2 = solve4 $ \m ->
+  [ validByr (m M.! "byr")
+    && validIyr (m M.! "iyr")
+    && validEyr (m M.! "eyr")
+    && validHgt (m M.! "hgt")
+    && validHcl (m M.! "hcl")
+    && validEcl (m M.! "ecl")
+    && validPid (m M.! "pid")
+  ]
+
+solve4 :: (M.Map String String -> [Bool]) -> [String] -> Int
+solve4 validationRules = foldl' countValidPassports 0
+  where
+    countValidPassports :: Int -> String -> Int
+    countValidPassports acc line = do
+       let fillMap m word | [k,v] <- splitOn ":" word = M.insert k v m
+           m = foldl' fillMap mempty (words line)
+           allFound = isJust (traverse (`M.lookup` m) day4Keys)
+       if allFound && and (validationRules m)
+         then
+           acc + 1
+         else
+           acc
+
+day4Keys =
+  [ "byr"
+  , "iyr"
+  , "eyr"
+  , "hgt"
+  , "hcl"
+  , "ecl"
+  , "pid"
+  ]
+
+ranged :: (Int,Int) -> String -> Bool
+ranged (x',y') str =
+  case readMaybe str of
+    Nothing -> False
+    Just (x :: Int) ->
+      x >= x' && x <= y'
+
+validHgt :: String -> Bool
+validHgt xs =
+  case readMaybe @Int (takeWhile isDigit xs) of
+    Nothing -> False
+    Just num ->
+      case reverse (take 2 (reverse xs)) of
+        "cm" -> ranged (150,193) (takeWhile isDigit xs)
+        "in" -> ranged (59,76) (takeWhile isDigit xs)
+        _ -> False
+
+validHcl :: String -> Bool
+validHcl ('#':str)
+  | and [ length str == 6
+        , all (\x -> x `elem` ['0'..'9'] || x `elem` ['a'..'f']) str
+        ] = True
+validHcl _ = False
+
+validEcl :: String -> Bool
+validEcl xs = xs `elem` ["amb", "blu","brn","gry","grn","hzl","oth"]
+
+validPid :: String -> Bool
+validPid xs = length (takeWhile isDigit xs) == 9
+
+validByr, validIyr, validEyr :: String -> Bool
+validByr = ranged (1920,2002)
+validIyr = ranged (2010,2020)
+validEyr = ranged (2020,2030)
+
 main :: IO ()
-main = day1 >> day2 >> day3
+main = do
+  day1
+  day2
+  day3
+  day4
 
