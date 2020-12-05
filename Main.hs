a4eb09421d13759124fe49cd499c3aa259dbf3f4
David Johnson <djohnson.m@gmail.com>
2020-12-05 00:12:20 -0600

    Day 5

--- Main.hs
+++ Main.hs
@@ -16,6 +16,14 @@ import           Data.Maybe
 import           Debug.Trace
 import           Text.Read
 
+main :: IO ()
+main = do
+  day1
+  day2
+  day3
+  day4
+  day5
+
 readInput :: String -> IO [Int]
 readInput name = fmap read . lines <$> readFile name
 
@@ -224,10 +232,50 @@ validByr = ranged (1920,2002)
 validIyr = ranged (2010,2020)
 validEyr = ranged (2020,2030)
 
-main :: IO ()
-main = do
-  day1
-  day2
-  day3
-  day4
+day5 :: IO ()
+day5 = do
+  ls <- lines <$> readFile "day5.txt"
+  let rowCols = fmap toRowCol ls
+      seatIds = sort (fmap toSeatId rowCols)
+  print $ day5pt1 rowCols
+  print $ day5pt2 seatIds
+
+day5pt1 :: [(Int,Int)] -> Int
+day5pt1 rowCols = maximum (fmap toSeatId rowCols)
+
+day5pt2 :: [Int] -> Int
+day5pt2 seatIds = findSeatId $ (flip zip =<< tail) seatIds
+  where
+    findSeatId (x:xs)
+      | fst x + 1 == snd x = findSeatId xs
+      | otherwise = fst x + 1
+
+toSeatId :: (Int,Int) -> Int
+toSeatId (row,col) = (row * 8) + col
+
+toRowCol :: String -> (Int,Int)
+toRowCol xs =
+  let
+    row = take 7 xs
+    col = take 3 (drop 7 xs)
+
+    (loRow, hiRow) = foldl' search (0,127) row
+    (loCol, hiCol) = foldl' search (0,7) col
+
+    search (lo,hi) 'F' = (lo, (hi + lo) `div` 2)
+    search (lo,hi) 'L' = (lo, (hi + lo) `div` 2)
+    search (lo,hi) 'B' = (1 + (hi + lo) `div` 2, hi)
+    search (lo,hi) 'R' = (1 + (hi + lo) `div` 2, hi)
+
+    exactRow =
+      if last row == 'F'
+      then loRow
+      else hiRow
+
+    exactCol =
+      if last col == 'R'
+      then hiCol
+      else loCol
+  in
+    (exactRow,exactCol)
 
